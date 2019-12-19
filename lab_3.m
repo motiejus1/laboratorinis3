214b4e23a69fe0f3ba40b0d63c62c1a7082b4df4
Motiejus <motiejus.baliukonis@vka.lt>
2019-12-20 00:39:45 +0200

    *aprasyti kai kurie elementai *ieskome minimumu ir funckijos maksimumu

--- lab_3.m
+++ lab_3.m
@@ -1,1 +1,19 @@
-%lab3
+clc
+clear
+
+%apsibreziam x
+x = 1/20:1/20:1;
+%originali funkcijos reiksme
+d =(1 + 0.6*sin((2*pi*x)/0.7)+0.3*sin(2*pi*x))/2;
+
+%ieskome d funckijos maximumu ir ju pozcijos
+[peaks,position] = findpeaks(d)
+
+%ieskome d funkcijos minimumu
+[lows,lposition] = islocalmin(d);
+
+%automatinis c parinkimas
+%c(1) = x(position(1));
+%c(2) = x(position(2));
+
+figure
